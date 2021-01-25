94e50b3711aaca4f7f03126bae8cc8fdcbf341f9
ArsenZhonbutaev <zhonbutaev.arsen@gmail.com>
2021-01-26 03:15:16 +0600

    added models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class ToDo(models.Model):
+    title = models.CharField(max_length=50)
+    subtitle = models.CharField(max_length=50)
+    price = models.CharField(max_length=10)
+    genre = models.CharField(max_length=20)
+    author = models.CharField(max_length=50)
+    year = models.CharField(max_length=10)
+    description = models.CharField(max_length=300)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
